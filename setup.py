7174ef1c44384eea8c8cc5f275aea608f2d61b7b
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2022-08-20 22:00:15 -0500

    Created the python package configuration file for this project

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Build-in imports
+from setuptools import setup, find_packages
+
+setup(
+    name="feature_hero_ml",
+    version="0.0.1",
+    description="""
+        This is a project that builds and implements a simple Feature Store using 
+        Feast, the main purpose of this project is to help in the training and 
+        deploying of Machine Learning models into production environments using the 
+        best MLOps practices.
+    """,
+    packages=find_packages()
+)
